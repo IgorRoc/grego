b56cc42bfbc703cb779461b42da3f17a6681a191
IgorRoc <igor_roc@hotmail.com.br>
2021-06-03 17:06:39 -0300

    [fix]: New location

--- map.js
+++ map.js
@@ -1,7 +1,9 @@
+const latitude = -14.789526546263717
+const longitude = -39.03310762615234
 // Initialize and add the map
 function initMap() {
 	// The location of Uluru
-	const uluru = { lat: -14.7895098, lng: -39.0329679 }
+	const uluru = { lat: latitude, lng: longitude }
 	// The map, centered at Uluru
 	const map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 18,
